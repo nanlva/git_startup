0a3581c280774e03bf2ba8302aa8ac4db31a409a
nanlva <1072597914@qq.com>
2023-07-02 09:37:50 +0800

    test remote conflict

--- dev_neobranch.c
+++ dev_neobranch.c
@@ -5,5 +5,6 @@ int main(void)
     printf("hello, this is neo branch dev");
     printf("test remote branch operation");
     printf("test remote branch operation again");
+    printf("test remote conflict");
     return 0;
 }
