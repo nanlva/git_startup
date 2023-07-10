e722e28808d66723ad8fe67b0984436e79a00942
nanlva <1072597914@qq.com>
2023-07-10 23:32:02 +0800

    git commit amend practise 1st

---
+++ push_f.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, this is git commit amend practise");
+    printf("this is first commit");
+    printf("this is second amend commit");
+    return 0;
+}
