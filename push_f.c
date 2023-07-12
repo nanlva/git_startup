9d65b15859fa10621fdb15cdcccb2392e99bc94c
nanlva <1072597914@qq.com>
2023-07-12 22:43:04 +0800

    modify on repo_neo2 master branch

--- push_f.c
+++ push_f.c
@@ -5,5 +5,6 @@ int main(void)
     printf("hello, this is git commit amend practise");
     printf("this is first commit");
     printf("this is second amend commit");
+    printf("modify on repo_neo2 master branch");
     return 0;
 }
