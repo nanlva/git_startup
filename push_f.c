2430f6f38ff8e1081b2a6c12ba7414741ef097c5
nanlva <1072597914@qq.com>
2023-07-12 22:37:19 +0800

    rebase test 1st

--- push_f.c
+++ push_f.c
@@ -6,5 +6,6 @@ int main(void)
     printf("this is first commit");
     printf("this is second amend commit");
     printf("modify on repo_neo2 master branch");
+    printf("rebase test 1st");
     return 0;
 }
